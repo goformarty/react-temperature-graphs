d17b018aa1d4096303a0d583f556f63031c7eb10
Marty Stepien <marty.stepien@gmail.com>
2018-06-01 18:00:45 +0100

    - form default submit prevented
    - web api key added

--- src/containers/searchbar.js
+++ src/containers/searchbar.js
@@ -4,14 +4,28 @@ export default class Searchbar extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {term: ''};
+		this.state = { term: '' };
+
+		this.onInputChange = this.onInputChange.bind(this);
 	}
+
+	onInputChange(event) {
+		console.log(event.target.value, this.state);
+		this.setState( {term: event.target.value} );
+	}
+
+	onFormSubmit(event) {
+		event.preventDefault();
+	}
+
 	render() {
 		return (
-			<form className="input-group">
+			<form onSubmit={this.onFormSubmit} className="input-group">
 				<input 
-					placeholder="Get a five-day forecast in your favourite city"
+					placehold er="Get a five-day forecast in your favourite city"
 					className="form-control"
+					value={this.state.term}
+					onChange={this.onInputChange}
 				/>
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-secondary" > Submit</button>
